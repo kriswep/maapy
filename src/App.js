f6d8c016899e24ee746b4f733ad9abf07397a2a2
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-07-29 13:57:09 +0200

    render some emojy markers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { StyleSheet, SafeAreaView } from 'react-native';
 import Map from './components/Map';
 
 export default class App extends React.Component {
+  renderMarkers() {
+    return this.props.places.map((place, i) => (
+      <Marker key={i} title={place.name} coordinate={place.coords} />
+    ))
+  }
   render() {
     return (
       <SafeAreaView style={styles.container}>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { MapView } from 'expo';
 
+const Marker = MapView.Marker
+
+const MyCustomMarkerView = ({title}) =>
+  (<View style={{
+    backgroundColor: 'rgba(255,255,255,0.7)',
+  }}>
+    <Text>{title}</Text>
+  </View>);
+
+
+const marker = [{
+  title: "😌",
+  coords: {
+    latitude: 37.88825,
+    longitude: -122.6324,
+  }},
+  {
+  title: "😻",
+  coords: {
+    latitude: 37.58825,
+    longitude: -122.3324,
+  }}
+  ]
+
 export default class App extends React.Component {
+  renderMarkers() {
+    return marker.map((marker, i) => (
+      <Marker key={i} coordinate={marker.coords}>
+        <MyCustomMarkerView {...marker} />
+      </Marker>
+    ))
+  }
+
   render() {
     return (
       <MapView
@@ -9,10 +42,12 @@ export default class App extends React.Component {
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitudeDelta: 0.7922,
+          longitudeDelta: 0.7421,
         }}
-      />
+      >
+        {this.renderMarkers()}
+      </MapView>
     );
   }
 }
